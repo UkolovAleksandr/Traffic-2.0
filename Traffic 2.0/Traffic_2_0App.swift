42cfc44cc0b903bed1d339b0840b347dab9eef4b
A.Ukolov <sanjrostov@mail.ru>
2022-04-28 23:18:09 +0300

    Initial Commit

---
+++ Traffic 2.0/Traffic_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_2_0App.swift
+//  Traffic 2.0
+//
+//  Created by Александр Уколов on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
